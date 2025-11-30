e92f09b368d3c8c496b03f735e92a528cb86520d
Hritic-kun <physics.hritic@gmail.com>
2025-11-30 23:55:08 +0530

    Data Transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,48 +3,55 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts', 'train.csv')
-    test_data_path: str= os.path.join('artifacts', 'test.csv')
-    raw_data_path: str= os.path.join('artifacts', 'data.csv')
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig() # sub oject creation
-        # three paths are created in artifacts folder when above line is executed
+        self.ingestion_config=DataIngestionConfig()
+
     def initiate_data_ingestion(self):
-        # if data stored in some database or cloud storage, code to fetch data will be written here
         logging.info("Entered the data ingestion method or component")
         try:
-            # this dataset is just for example, you can replace it with your own dataset
-            # it can be read from database or cloud storage also or api
-            df = pd.read_csv('notebook/data/stud.csv')
+            df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
-            
-            # since aritfacts folder may not be present, we create it
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
-            # saving raw data
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            # train test split
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
             logging.info("Train test split initiated")
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            # saving train and test data
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-            
-            logging.info("Ingestion of the data is completed")
-            # returning the file paths
-            return (
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Inmgestion of the data iss completed")
+
+            return(
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path
+
             )
         except Exception as e:
-           raise CustomException(e, sys)
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+
